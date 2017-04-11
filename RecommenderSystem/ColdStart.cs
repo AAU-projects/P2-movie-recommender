c84f3d3a03a2544ad6f2d7955be1d0a6e5a4d96e
greenhawky <mobilsp@live.dk>
2017-04-11 13:15:08 +0200

    coldstart

---
+++ RecommenderSystem/ColdStart.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecommenderSystem
+{
+    class ColdStart : Menu
+    {
+        public ColdStart(string title) : base(title)
+        {
+        }
+
+        public override void Select()
+        {
+            Console.Clear();
+            Console.WriteLine($"Choose more than 10 Movies \nPress enter to continue");
+            Console.ReadLine();
+            Console.Clear();
+
+            FindUnratedMovies(10); 
+            
+        }
+
+        private void FindUnratedMovies(int numberOfMovies)
+        {
+            List<int> usedNumbers = new List<int>();
+            int totalNumberOfMovies = MySqlCommands.NumberOfRowsInTable("imdbdata");
+           
+            do
+            {
+                usedNumbers = GenerateRandomNumber(totalNumberOfMovies, numberOfMovies, usedNumbers);
+                
+            } while (true);
+
+        }
+
+        private List<int> GenerateRandomNumber(int totalNumberOfMovies, int numberOfGeneratedNumbers, List<int> usedNumbers)
+        {
+            Random random = new Random();
+            int number = 0;
+
+            for (int i = 0; i < numberOfGeneratedNumbers; i++)
+            {
+                do
+                {
+                    number = random.Next(1, totalNumberOfMovies);
+                } while (usedNumbers.Contains(number));
+
+                usedNumbers.Add(number);
+            }
+            return usedNumbers; 
+        }
+    }
+}
