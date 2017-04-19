9767977aa07d1ed195c780167b60abaf5c67a432
Gustav Brodersen <gustavbrodersen@gmail.com>
2017-04-19 12:16:57 +0200

    Gruppearbejde 19-04

--- RecommenderSystem/RateMovie.cs
+++ RecommenderSystem/RateMovie.cs
@@ -8,12 +8,19 @@ namespace RecommenderSystem
 {
     class RateMovie : MenuItemBase
     {
-        public RateMovie(string title, ConsoleColor color) : base(title, color)
-        {}
+        private int _movieID;
+        private string _enumvalue;
+
+        public RateMovie(string title, ConsoleColor color, string enumvalue, int movieID) : base(title, color)
+        {
+            _movieID = movieID;
+            _enumvalue = enumvalue;
+        }
         
         public override void Select()
         {
             Console.Write("Movie Rated! \t");
+            MySqlCommands.RateMovie(_movieID, _enumvalue);
         } 
     }
 }
